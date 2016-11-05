db2b3eaf0791399a49f26c679e567409ff9ebf64
SommaiAndroid <phumjumpa@gmail.com>
2016-11-05 14:40:21 +0700

    check space

--- app/src/main/java/phumjumpa/sommai/easymicro/SignActivity.java
+++ app/src/main/java/phumjumpa/sommai/easymicro/SignActivity.java
@@ -2,12 +2,65 @@ package phumjumpa.sommai.easymicro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class SignActivity extends AppCompatActivity {
+// Explicit
+    private EditText nameEditText,userEditText, passwordEditText;
+    private ImageView imageView;
+    private Button button;
+    private String nameString,userString, passwordString;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
-    }
-}
+
+        //bind widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.button3);
+
+        // Signup Controller
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              // get value from edit text
+
+                nameString = nameEditText.getText().toString().trim();
+                userString = userEditText.getText().toString().trim();
+                passwordString = passwordEditText.getText().toString().trim();
+
+
+                // Check space
+                if (nameString.equals("") || userString.equals("") || passwordString.equals(""))
+                {
+                    // Have space
+                    Log.d("5novV1", "Have Space");
+
+                } // if
+
+
+
+
+
+            } // onclick
+
+
+        });
+
+
+
+    } //Main method
+} // Main Class
+
