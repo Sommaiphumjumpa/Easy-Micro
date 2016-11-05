776268521d621a4e856376d27efb53541793363c
SommaiAndroid <phumjumpa@gmail.com>
2016-11-05 11:59:03 +0700

    intent from master to signup

--- app/src/main/java/phumjumpa/sommai/easymicro/MainActivity.java
+++ app/src/main/java/phumjumpa/sommai/easymicro/MainActivity.java
@@ -1,13 +1,35 @@
 package phumjumpa.sommai.easymicro;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    // explicit การประกาศตัวแปร
+    private Button signInButton, signUpButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton = (Button) findViewById(R.id.button2);
+
+        // Signup controller
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignActivity.class));
+            }
+        });
+
+
+
+    }// Main method
+} // main class
