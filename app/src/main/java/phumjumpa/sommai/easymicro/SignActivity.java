42ca9ce91142bdac0239d60df307f9ca3f10934a
SommaiAndroid <phumjumpa@gmail.com>
2016-11-06 13:43:42 +0700

    upload string

--- app/src/main/java/phumjumpa/sommai/easymicro/SignActivity.java
+++ app/src/main/java/phumjumpa/sommai/easymicro/SignActivity.java
@@ -1,10 +1,13 @@
 package phumjumpa.sommai.easymicro;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +16,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import org.jibble.simpleftp.SimpleFTP;
+
+import java.io.File;
 
 public class SignActivity extends AppCompatActivity {
     // Explicit
@@ -71,7 +85,8 @@ public class SignActivity extends AppCompatActivity {
                     _myAlert1.myDiaLog();
                 } else {
                     // upload to server
-
+                    uploadImage();
+                    upLoadString();
                 }
 
             } // onclick
@@ -94,6 +109,85 @@ public class SignActivity extends AppCompatActivity {
 
 
     } //Main method
+
+    private class AddNewUser extends AsyncTask<String, Void, String> {
+      //Explicit
+       private Context context;
+
+        public AddNewUser(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+                OkHttpClient okHttpClient=new OkHttpClient();
+                RequestBody requestBody = new FormEncodingBuilder()
+                        .add("isAdd", "true")
+                        .add("Name", nameString)
+                        .add("User", userString)
+                        .add("Password", passwordString)
+                        .add("Image", imageString)
+                        .build();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(strings[0]).post(requestBody).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                Log.d("6novV2", "e do inback" + e.toString());
+                return null;
+            }
+
+
+        } //doinback
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d("6NovV2","Result==>" + s);
+
+
+        } // Onpost
+    }// Add new user Class
+    private void upLoadString() {
+        imageString="http://swiftcodingthai.com/mic/Images"+ imageNameString;
+        MyConstante myConstante=new MyConstante();
+        AddNewUser addNewUser = new AddNewUser(SignActivity.this);
+        addNewUser.execute((myConstante.getUrlAddUser()));
+
+
+    }
+
+    private void uploadImage() {
+        // Change Policy
+        StrictMode.ThreadPolicy threadPolicy =
+                new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy((threadPolicy));
+
+        try {
+            MyConstante myConstante = new MyConstante();
+            SimpleFTP simpleFTP = new SimpleFTP();
+            simpleFTP.connect(myConstante.getHostString(),
+                    myConstante.getPortAnInt(),
+                    myConstante.getUserString(),
+                    myConstante.getPasswordString());
+            simpleFTP.bin();
+            simpleFTP.cwd("Images");
+            simpleFTP.stor(new File(imagePathString));
+            simpleFTP.disconnect();
+
+            Toast.makeText(SignActivity.this,"Upload Image Finish",Toast.LENGTH_SHORT).show();
+
+
+        } catch (Exception e) {
+
+            Log.d("6novV1", "ftp error==> " + e.toString());
+        }
+
+    }// upload image
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -119,7 +213,7 @@ public class SignActivity extends AppCompatActivity {
 
             // Find Name of image choose
             imageNameString = imagePathString.substring(imagePathString.lastIndexOf("/"));
-            Log.d("6novV1","Path==> " + imageNameString);
+            Log.d("6novV1", "Path==> " + imageNameString);
 
         } // if
 
@@ -128,17 +222,17 @@ public class SignActivity extends AppCompatActivity {
 
     private String myFindPath(Uri uri) {
 
-        String result =null;
-        String [] strings= {MediaStore.Images.Media.DATA};
-        Cursor cursor = getContentResolver().query(uri,strings,null,null,null);
+        String result = null;
+        String[] strings = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getContentResolver().query(uri, strings, null, null, null);
 
-        if (cursor!=null) {
+        if (cursor != null) {
             cursor.moveToFirst();
             int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
             result = cursor.getString(index);
 
         } else {
-            result=uri.getPath();
+            result = uri.getPath();
         } // if
 
         return result;
